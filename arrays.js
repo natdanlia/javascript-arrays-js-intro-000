c22cafe70caa09469c322bd563b443c72056321d
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-15 18:22:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+
+
+// function addElementToBeginningOfArray(arr, el) {
+//   return arr.unshift(el)
+// }
+// 
+// 
+// function destructivelyAddElementToBeginningOfArray(arr, el){
+//   
+// } 
