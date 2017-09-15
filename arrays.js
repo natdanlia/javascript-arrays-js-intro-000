7e6da6b937e0e24b6c0f1789db58e4efcb229ff7
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-15 18:33:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,5 +8,5 @@ function addElementToBeginningOfArray(array, element) {
 
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element)
+  return array.unshift('element')
 }
