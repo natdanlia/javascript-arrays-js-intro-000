80d9775247daa1bcf5a51e45b7e37c26e8888a3b
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-15 18:24:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,8 +5,8 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 // function addElementToBeginningOfArray(arr, el) {
 //   return arr.unshift(el)
 // }
-// 
-// 
+//
+//
 // function destructivelyAddElementToBeginningOfArray(arr, el){
-//   
-// } 
+//
+// }
